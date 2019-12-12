3216fda4ad5ae3b8eb074686b3c36e5c2e2fb0db
miguel aguilar <maguilarv@unprg.edu.pe>
2019-12-11 20:17:37 -0500

    se agrego los 15 ejercicios sobre cadenas tipo "indexacion" de mi compañero

---
+++ arroyo/indexacion_de_cadenas.py
@@ -0,0 +1,63 @@
+#digitamos numeros para guiarnos
+#                10        20
+#       012345678901234567890123456
+letras="ABCDEFGHIJKLMNOPQRSTUVWXYZÑ"
+
+#ejercicio 01 ( MI MOCHILA ES NEGRA)
+print(letras[12] +letras[8] + " " + letras[12] + letras[14] + letras[2] + letras[7] + letras[8]
+      + letras[11] + letras[0] +" "+ letras[4] + letras[18] + " " + letras[13] +letras[4] + letras[6] + letras[17]+letras[0])
+
+#ejercicio 02 ( MI POLO ES DE COLOR AZUL)
+print(letras[12] +letras[8] + " " + letras[15] + letras[14] + letras[11] +  letras[14] + " "+ letras[4] +  letras[18] + " "
+      + letras[3] + letras[4] +" "+ letras[2] + letras[14]+ letras[11] + letras[14]  + letras[17] + " " + letras[0] + letras[25] + letras[20]+letras[11])
+
+#ejercicio 03 ( MI NOMBRE ES GONZALO)
+print(letras[12] +letras[8] +" " + letras[13] + letras[14] + letras[12] + letras[1] + letras[17]
+      + letras[4] +" " +  letras[4] + letras[18] +" "+ letras[6] +  letras[14] +letras[13] + letras[25] + letras[0]+letras[11]+ letras[14])
+
+#ejercicio 04 ( MI APELLIDO ES ARROYO)
+print(letras[12] +letras[8] + " "+ letras[0] + letras[15] + letras[4] + letras[11] + letras[11]
+      + letras[8] + letras[3] + letras[14]  + letras[4] +  letras[18] +letras[0] + letras[17] + letras[17] + letras[14]+ letras[24]  + letras[14])
+
+#ejercicio 05 ( MI UNIVERSIDAD ES NACIONAL)
+print(letras[12] +letras[8] + " " + letras[20] + letras[13] + letras[8] + letras[21] + letras[4]
+      + letras[17] + letras[18] + letras[8] + letras[3] +  letras[0] +letras[3] + " "+letras[4] + letras[18]+" " + letras[13] + letras[0] + letras[2] + letras[8] + letras[14] + letras[13]+ letras[0] + letras[11])
+
+#ejercicio 06 ( MI PERU QUERIDO)
+print(letras[12] +letras[8] + " " + letras[15] + letras[4] + letras[17] + letras[20] + " " + letras[16]
+      + letras[20] + letras[4] + letras[17] + letras[8] +  letras[3] +letras[14] )
+
+#ejercicio 07 ( ME GUSTA CAMINAR)
+print(letras[12] +letras[4] + " " + letras[6] + letras[20] + letras[18] + letras[19] + letras[0] + " "
+      + letras[2] + letras[0] +letras[12] + letras[8] + letras[13] +letras[0] +  letras[17])
+
+#ejercicio 08 (TODO PERRO ES MAMIFERO)
+print(letras[19] +letras[14] +letras[3] +letras[14]+ " " + letras[15] + letras[4] + letras[17] + letras[17] + letras[14]
+      + " " + letras[4] + letras[18] +" "+ letras[12] + letras[0] +  letras[12] +letras[8] + letras[5] + letras[4]+letras[17]+ letras[14])
+
+#ejercicio 09 ( MAÑANA NO HAY CLASES)
+print(letras[12] +letras[0] +  letras[26] + letras[0] + letras[13] + letras[0] +" "+ letras[13]
+      + letras[14] + " " + letras[7] + letras[0] + letras[24] + " " + letras[2] +letras[11] + letras[0] + letras[18]+letras[4] + letras[18])
+
+#ejercicio 10 ( MI PERRO ES MACHO)
+print(letras[12] +letras[8] + " " + letras[15] + letras[4] + letras[17] + letras[17] + letras[14]+ " "
+      + letras[4] + letras[18] + " "+letras[12] + letras[0] +  letras[2] +letras[7] + letras[14] )
+
+#ejercicio 11 ( SOY ALTO)
+print(letras[18] +letras[14] +  letras[24]+ " " + letras[0] + letras[11] + letras[19] + letras[14] )
+
+#ejercicio 12 ( VIVO EN FERREÑAFE)
+print(letras[21] +letras[8] +  letras[21] + letras[14] +" "+ letras[4] + letras[13] +" "+ letras[5]
+      + letras[4] + letras[17] +letras[17] + letras[4] +  letras[26] +letras[0] + letras[5] + letras[4])
+
+#ejercicio 13 ( SOY MUY AMABLE)
+print(letras[18] +letras[14] +  letras[24]+ " "  + letras[12] + letras[20] + letras[24] +" "+ letras[0]
+      + letras[12] + letras[0] +letras[1] + letras[11] +  letras[4] )
+
+#ejercicio 14 ( EL FUTBOL ES MY PASION)
+print(letras[4] +letras[11] + " "+  letras[5]+  letras[20] + letras[19] + letras[1] +letras[14]
+      + letras[11] + " "+ letras[4] +letras[18] +" "+  letras[12] +  letras[8]+" "+letras[15] +letras[0] + letras[18] +  letras[8]+ letras[14] +  letras[13]  )
+
+#ejercicio 15 ( YA TERMINE)
+print(letras[24] +letras[0] +" "+  letras[19]+ letras[4] + letras[17]
+      + letras[12] + letras[8] +letras[13] + letras[4]  )
