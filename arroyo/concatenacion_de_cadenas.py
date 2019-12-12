bfe4e1dfc0ef86a8471c3d09c053372fb2f45ae0
miguel aguilar <maguilarv@unprg.edu.pe>
2019-12-11 19:58:35 -0500

    se agrego los 15 ejercicios sobre cadenas tipo "concatenacion" de mi compañero

---
+++ arroyo/concatenacion_de_cadenas.py
@@ -0,0 +1,137 @@
+#EJERCICIOS DE CONCATENACION
+#jercicio1
+#    0         10        20        30        40
+#    01234567890123456789012345678901234567890123456789
+wtr="COMO ESTAS TE ESTOY ESPERANDO EN CHICLAYO TU DONDE"
+#concatenamos la oracion previaa
+print(wtr[11] + wtr[1] + wtr[2] + wtr[3]  + wtr[4] +wtr[33]+ wtr[34]+ wtr[35]+wtr[33] + wtr[34] +wtr[38])
+#fin_concatenacion
+
+#ejercicio2
+#Concatenar las cadenas edad y nombre
+edad=18
+nombre="gonzalo"
+
+print( nombre + " tiene " + str(edad)+ " años ")
+#fin_concatenacion
+
+#ejercicio3
+#Concatenar las cadenas nombre y fruta
+nombre="\"GONZALO\""
+fruta="manzana"
+
+estatura="alto"
+print( "Al joven " + nombre + " le gusta la fruta de  "+ fruta + "\n" + "y es muy  "+ estatura)
+#fin_concatenacion
+#ejercicio4
+#Concatenar las cadenas cancion, autor y pais
+cancion="\"Dime que no\""
+autor="Ricardo Arjona"
+pais="Guatemala"
+
+print( "El cantante " +autor+  " es autor de la cancion "+ cancion+ " y nacio en el pais de " + pais)
+#fin_concatenacion
+
+#ejercicio5
+#Concatenar las cadenas pais1 y caracteristica
+pais1="\"PERU\""
+caracteristica="gastronomia"
+
+print( "El " + pais1+  " se caracteriza por su rica "+ caracteristica )
+#fin_concatenacion
+
+#ejercicio6
+#Concatenar las cadenas universidad, departamento  y caracteristica
+universidad="\"Pedro Ruiz Gallo\""
+departamento="Lambayeque"
+caracteristica1="prestigiosa"
+
+print( "La universidad "+universidad+ "  se encuentra en el departamento de " + departamento+ "  y es muy " + caracteristica1)
+#fin_concatenacion
+
+#ejercicio7
+#Concatenar las cadenas universidad, carrera y curso
+universidad="\"Pedro Ruiz Gallo\""
+carrera="Ingenieria electronica"
+curso="programacion"
+
+print("Yo estudio la carrera de "+carrera+  " en la universidad " +universidad+ " y llevo el curso de " +curso)
+#fin_concatenacion
+
+#ejercicio8
+#Concatenar las cadenas curso1, curso2, curso3 y  curso4
+curso1="Matematica basica"
+curso2="Analisis matematico"
+curso3="programacion"
+curso4="quimica"
+
+print("los curso que llevo en el primer ciclo en la universidad son: "+curso1 + ", " + curso2+ ", " + curso3+ " y " +curso4)
+#fin_concatenacion
+
+#ejercicio9
+#Concatenar las cadenas edad y nombre
+departamento1="Lambayeque"
+prov1="Ferreñafe"
+prov2="Chiclayo"
+prov3="Lambayeque"
+
+print("El departamento de "+departamento1+ " tiene tres provincias: " +prov1+", "+prov2+ " y "+prov3)
+#fin_concatenacion
+
+#ejercicio10
+#Concatenar las cadenas departament1, prov1, prov2 y prov3
+departamento1="Lambayeque"
+prov1="Ferreñafe"
+prov2="Chiclayo"
+prov3="Lambayeque"
+
+print("El departamento de "+departamento1+ " tiene tres provincias: " +prov1+", "+prov2+ " y "+prov3)
+#fin_concatenacion
+
+#ejercicio11
+#Concatenar las cadenas departament1, prov1, prov2 y prov3
+departamento1="Lambayeque"
+prov1="Ferreñafe"
+prov2="Chiclayo"
+prov3="Lambayeque"
+
+print("El departamento de "+departamento1+ " tiene tres provincias: " +prov1+", "+prov2+ " y "+prov3)
+#fin_concatenacion
+
+#ejercicio12
+#Concatenar las cadenas color1 y color2
+color1="Ferreñafe"
+color2="Chiclayo"
+
+
+print("Me gustaria tener una moto "+ color1+  "y un auto "+ color2)
+#fin_concatenacion
+
+#ejercicio13
+#Concatenar las cadenas cantantes y nacionalidad
+cantantes="Chino y Nacho"
+nacionalidad="venezolana"
+
+
+print("Los cantantes "+cantantes+  " son de nacionalidad "+ nacionalidad)
+#fin_concatenacion
+
+#ejercicio14
+#Concatenar las cadenas actor, peli1 y peli2
+actor="Dwayne Johnson"
+peli1="El rey escorpion"
+peli2="Guardianes de la bahia"
+
+
+print("El actor "+actor+ " ha actuado en las peliculas de " + peli1+ " y "+ peli2)
+#fin_concatenacion
+
+#ejercicio15
+#Concatenar las cadenas esta1, esta2, esta3 y esta4
+esta1="verano"
+esta2="invierno"
+esta3="primavera"
+esta4="otoño"
+
+
+print("El planeta tierra presenta cuatro estaciones: "+esta1+ ", "+ esta2+ ", "+esta3+ " y "+ esta4)
