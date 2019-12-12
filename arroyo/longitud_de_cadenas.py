98409391e4f073aa9ffeeacf3fe29bf96c8b7362
miguel aguilar <maguilarv@unprg.edu.pe>
2019-12-11 20:05:25 -0500

    se agrego los 15 ejercicios sobre cadenas tipo "longitud" de mi compañero

---
+++ arroyo/longitud_de_cadenas.py
@@ -0,0 +1,90 @@
+#medidremos la longitud de las cadenas
+#ejercicio 01 - longitud de la frase
+#Hallando la longitud de la oracion
+cadn="Hola, que tal, como estas"
+print("La longitud de la cadena es;", len(cadn))
+#fin_ejercicio
+
+#ejercicio 02- longitud de la frase
+#Hallando la longitud de la oracion
+cadn="Universidad Nacional Pedro Ruiz Gallo"
+print("La longitud de la cadena es;", len(cadn))
+#fin_ejercicio
+
+#ejercicio 03- longitud de la frase
+#Hallando la longitud de la oracion
+cadn="La universidad aun no esta licenciada"
+print("La longitud de la cadena es;", len(cadn))
+#fin_ejercicio
+
+#ejercicio 04- longitud de la frase
+#Hallando la longitud de la oracion
+cadn="Mi curso favorito es programacio "
+print("La longitud de la cadena es;", len(cadn))
+#fin_ejercicio
+
+#ejercicio 05- longitud de la frase
+#Hallando la longitud de la oracion
+cadn="Mi compañeros son muy buenos"
+print("La longitud de la cadena es;", len(cadn))
+#fin_ejercicio
+
+#ejercicio 06- longitud de la frase
+#Hallando la longitud de la oracion
+cadn="Ya termina el años 2019"
+print("La longitud de la cadena es;", len(cadn))
+#fin_ejercicio
+
+#ejercicio 07- longitud de la frase
+#Hallando la longitud de la oracion
+cadn="Tengo bastantes tareas"
+print("La longitud de la cadena es;", len(cadn))
+#fin_ejercicio
+
+#ejercicio 08- longitud de la frase
+#Hallando la longitud de la oracion
+cadn="Necesito vacaciones, yaaa"
+print("La longitud de la cadena es;", len(cadn))
+#fin_ejercicio
+
+#ejercicio 09- longitud de la frase
+#Hallando la longitud de la oracion
+cadn="Mi perra se llama pelusa"
+print("La longitud de la cadena es;", len(cadn))
+#fin_ejercicio
+
+#ejercicio 10- longitud de la frase
+#Hallando la longitud de la oracion
+cadn="Las personas son muy imprescindibles"
+print("La longitud de la cadena es;", len(cadn))
+#fin_ejercicio
+
+#ejercicio 11- longitud de la frase
+#Hallando la longitud de la oracion
+cadn="El peru se encuentra en el continente americano"
+print("La longitud de la cadena es;", len(cadn))
+#fin_ejercicio
+
+#ejercicio 12- longitud de la frase
+#Hallando la longitud de la oracion
+cadn="Lambayeque es el segundo departamento mas pequeños"
+print("La longitud de la cadena es;", len(cadn))
+#fin_ejercicio
+
+#ejercicio 13- longitud de la frase
+#Hallando la longitud de la oracion
+cadn="Yo estudio en lafacultad de ciencias fisicas y matematicas"
+print("La longitud de la cadena es;", len(cadn))
+#fin_ejercicio
+
+#ejercicio 14- longitud de la frase
+#Hallando la longitud de la oracion
+cadn="Su apellido del presidente del peru es vizcarra"
+print("La longitud de la cadena es;", len(cadn))
+#fin_ejercicio
+
+#ejercicio 15- longitud de la frase
+#Hallando la longitud de la oracion
+cadn="La marca de mi celular es HUAWEI"
+print("La longitud de la cadena es;", len(cadn))
+#fin_ejercicio
