2961fe18a00c5efb35f2d54bbdf26b880c842dd2
miguel aguilar <maguilarv@unprg.edu.pe>
2019-12-11 20:00:56 -0500

    se agrego los 15 ejercicios sobre cadenas tipo "busqueda" de mi compañero

---
+++ arroyo/busqueda_de_cadenas.py
@@ -0,0 +1,91 @@
+#EJERCICIOS DE BUSQUEDA
+
+#EJERCICIO 01
+#BUSCAMOS LA UBICAION DE LA PALABRA UNIVERSIDAD
+cadena="Pedro Ruiz Gallo es una universidad"
+#busqueda
+print("la palabra se encuentra en el indice positivo ; ", cadena.find("universidad"))
+
+#EJERCICIO 02
+#BUSCAMOS LA UBICAION DE LA PALABRA GONZALO
+cadena="MI NOMBRE ES GONZALO"
+#busqueda
+print("la palabra se encuentra en el indice positivo ; ", cadena.find("GONZALO"))
+
+#EJERCICIO 03
+#BUSCAMOS LA UBICAION DE LA PALABRA EN
+cadena="LAMBAYEQUE SE ENCUENTRA EN EL PAIS LLAMADO PERU"
+#busqueda
+print("la palabra se encuentra en el indice positivo ; ", cadena.find("EN"))
+
+#EJERCICIO 04
+#BUSCAMOS LA UBICAION DE LA PALABRA PRIMER
+cadena="ME GUSTA EL CURSO DE PROGRAMACION EN EL PRIMER CICLO"
+#busqueda
+print("la palabra se encuentra en el indice positivo ; ", cadena.find("PRIMER"))
+
+#EJERCICIO 05
+#BUSCAMOS LA UBICAION DE LA PALABRA NIÑEZ
+cadena="LA CANCION PREFERIDA DE MI NIÑEZ FUE LA VACA LOLA"
+#busqueda
+print("la palabra se encuentra en el indice positivo ; ", cadena.find("NIÑEZ"))
+
+#EJERCICIO 06
+#BUSCAMOS LA UBICAION DE LA PALABRA M
+cadena="MI GATO LLEVA POR NOMBRE COCO"
+#busqueda
+print("la palabra se encuentra en el indice positivo ; ", cadena.find("M"))
+
+#EJERCICIO 07
+#BUSCAMOS LA UBICAION DE LA PALABRA U
+cadena="MI COLOR PREFERIDO ES EL AZUL Y BLANCO"
+#busqueda
+print("la palabra se encuentra en el indice positivo ; ", cadena.find("U"))
+
+#EJERCICIO 08
+#BUSCAMOS LA UBICAION DE LA PALABRA DE
+cadena="ESPERO APROBAR EL CURSO DE PROGRAMACION Y LLEVARLO EN VACACIONAL"
+#busqueda
+print("la palabra se encuentra en el indice positivo ; ", cadena.find("DE"))
+
+#EJERCICIO 09
+#BUSCAMOS LA UBICAION DE LA PALABRA DIA
+cadena="HOY EL DIA ESTUVO SOLEADO"
+#busqueda
+print("la palabra se encuentra en el indice positivo ; ", cadena.find("DIA"))
+
+#EJERCICIO 10
+#BUSCAMOS LA UBICAION DE LA PALABRA ESTA
+cadena="LA UNIVERSIDAD AUN NO ESTA OICENCIADA"
+#busqueda
+print("la palabra se encuentra en el indice positivo ; ", cadena.find("ESTA"))
+
+#EJERCICIO 11
+#BUSCAMOS LA UBICAION DE LA PALABRA TARDE
+cadena="MAÑANA EN LA TARDE JUGAREMOS FUTBOL"
+#busqueda
+print("la palabra se encuentra en el indice positivo ; ", cadena.find("TARDE"))
+
+#EJERCICIO 12
+#BUSCAMOS LA UBICAION DE LA PALABRA FUTBOL
+cadena="MI DEPORTE FAVORITO S EL FUTBOL YEL BASQUETBOL"
+#busqueda
+print("la palabra se encuentra en el indice positivo ; ", cadena.find("FUTBOL"))
+
+#EJERCICIO 13
+#BUSCAMOS LA UBICAION DE LA PALABRA COLOR
+cadena="MI MOCHILA ES DE COLOR NEGRO CON BLANCO"
+#busqueda
+print("la palabra se encuentra en el indice positivo ; ", cadena.find("COLOR"))
+
+#EJERCICIO 14
+#BUSCAMOS LA UBICAION DE LA PALABRA METRO
+cadena="ACTUALMENTE MI ESTATURA ES UN METRO SETENTA"
+#busqueda
+print("la palabra se encuentra en el indice positivo ; ", cadena.find("METRO"))
+
+#EJERCICIO 15
+#BUSCAMOS LA UBICAION DE LA PALABRA MAS
+cadena="LA NAVIDAD ESTA CADA VEZ MAS CERCA"
+#busqueda
+print("la palabra se encuentra en el indice positivo ; ", cadena.find("MAS"))
