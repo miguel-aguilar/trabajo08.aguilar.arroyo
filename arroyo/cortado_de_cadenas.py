7976de98f3e4a9782aafc1aebaa21fad74bc6d13
miguel aguilar <maguilarv@unprg.edu.pe>
2019-12-11 19:56:32 -0500

    se agrego los 15 ejercicios sobre cadenas tipo "cortado" de mi compañero

---
+++ arroyo/cortado_de_cadenas.py
@@ -0,0 +1,203 @@
+#jercisio1
+# EJERCICIO
+#Las frases seran cortadas
+#             10        20
+#    0123456789012345678901234567
+msg="LA VIDA TE PONDRA OBSTACULOS"
+
+
+print(msg[16:10:-1])
+print(msg[3:7])
+print(msg[7:8])
+
+#jercisio2
+# EJERCICIO
+#Las frases seran cortadas
+#             10        20         30        40
+#    01234567890123456789012345678910123456789012345
+msg="LO AQUELLO IMPOSIBLE ES AQUELLO QUE NO INTENTAS"
+
+print(msg[11:20])
+print(msg[45:37:-1])
+print(msg[15:20])
+print(msg[24:28])
+print(msg[23:32:-1])
+
+
+#jercisio3
+# EJERCICIO
+#Las frases seran cortadas
+#             10        20        30        40        50
+#    012345678901234567890123456789012345678901234567890123
+msg="SI EL PLAN NO FUNCIONA, CAMBIA EL PLAN PERO NO LA META"
+
+print(msg[14:22])
+print(msg[39:43])
+print(msg[39:46])
+print(msg[21:13:-1])
+print(msg[10:11])
+
+#jercisio4
+# EJERCICIO
+#Las frases seran cortadas
+#             10        20        30        40
+#    012345678901234567890123456789012345678901234567
+msg="NUNCA ABANDONES TUS SUEÑOS, DUERME 5 MINUTOS MAS"
+
+print(msg[10:15])
+print(msg[22:26])
+print(msg[14:5:-1])
+print(msg[::-1])
+print(msg[36:35:-1])
+
+#jercisio5
+# EJERCICIO
+#Las frases seran cortadas
+#             10        20        30
+#    012345678901234567890123456789012345
+msg="LO QUE NO TE MATA TE HACE MAS FUERTE"
+print(msg[16:13:-1])
+print(msg[24:21:-1])
+print(msg[3:6])
+print(msg[25:26])
+
+
+#jercisio6
+# EJERCICIO
+#Las frases seran cortadas
+#Las frases seran cortadas
+#             10        20        30
+#    012345678901234567890123456789012345
+msg="EL EXITO MAS GRANDE ES LA ACEPTACION"
+
+print(msg[35:30:-1])
+print(msg[7:2:-1])
+print(msg[30:36])
+print(msg[3:7])
+print(msg[25:26:-1])
+
+
+#jercisio7
+# EJERCICIO
+#Las frases seran cortadas
+#             10        20        30
+#    012345678901234567890123456789012345
+msg="DALE LUZ Y LA OSCURIDAD DESAPARECERA"
+
+print(msg[0:4])
+print(msg[14:23])
+print(msg[9:10])
+print(msg[24:36])
+print(msg[4:5])
+
+
+#jercisio8
+# EJERCICIO
+#Las frases seran cortadas
+#             10        20        30
+#    012345678901234567890123456789012
+msg="LA MENTE ES EL LIMITE DE TI MISMO"
+
+print(msg[32:27:-1])
+print(msg[20:14:-1])
+print(msg[9:14])
+print(msg[22:27])
+print(msg[2:3])
+
+#jercisio9
+# EJERCICIO
+#Las frases seran cortadas
+#             10        20
+#    012345678901234567890123
+msg="HAZ SIEMPRE LO IMPOSIBLE"
+
+print(msg[10:3:-1])
+print(msg[2:0:-1])
+print(msg[15:24])
+print(msg[6:11])
+print(msg[14:15])
+
+#jercisio10
+# EJERCICIO
+#Las frases seran cortadas
+#             10        20        30        40
+#    01234567890123456789012345678901234567890123456
+msg="HOY SALUDA A TODOS CON UNA SONRISA EN EL ROSTRO"
+
+print(msg[17:12:-1])
+print(msg[30:34])
+print(msg[46:40:-1])
+print(msg[9:3:-1])
+print(msg[19:26])
+print(msg[3:4])
+
+
+#jercisio11
+# EJERCICIO
+#Las frases seran cortadas
+#             10        20        30
+#    0123456789012345678901234567890123456789
+msg="COMPITE CONTRA TI MISMO, NO COMTRA OTROS"
+
+print(msg[39:34:-1])
+print(msg[8:14])
+print(msg[15:23])
+print(msg[26:24:-1])
+print(msg[24:25])
+
+
+#jercisio12
+# EJERCICIO
+#Las frases seran cortadas
+#             10        20
+#    0123456789012345678901234567890
+msg="PERU CAMPEON DE LA COPA AMERICA"
+
+print(msg[30:23:-1])
+print(msg[11:4:-1])
+print(msg[0:4])
+print(msg[13:23])
+print(msg[27:31])
+print(msg[4:5])
+
+
+#jercisio13
+# EJERCICIO
+#Las frases seran cortadas
+#             10        20
+#    01234567890123456789012345678
+msg="PERU, EL MEJOR PAIS QUE EXITE"
+
+print(msg[6:8])
+print(msg[9:14])
+print(msg[20:23])
+print(msg[0:4])
+print(msg[19:20])
+
+#jercisio14
+# EJERCICIO
+#Las frases seran cortadas
+#             10        20        30
+#    0123456789012345678901234567890123456
+msg="UNIVERSIDAD NACIONAL PEDRO RUIZ GALLO"
+
+print(msg[25:20])
+print(msg[36:32])
+print(msg[0:11])
+print(msg[20:11:-1])
+print(msg[27:31])
+print(msg[26:27])
+
+#jercisio15
+# EJERCICIO
+#Las frases seran cortadas
+#             10        20        30        40
+#    012345678901234567890123456789012345678901234
+msg="IDEAS GRANDES NO FUNCIONAS EN LETRAS PEQUEÑAS"
+
+print(msg[4:1:-1])
+print(msg[37:42])
+print(msg[9:13])
+print(msg[22:26])
+print(msg[8:13])
+print(msg[5:6])
