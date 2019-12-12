2d99abd53770273d7280e1be4b5619ecd90d2769
miguel aguilar <maguilarv@unprg.edu.pe>
2019-12-11 19:40:44 -0500

    se agrego los 15 ejercicios sobre cadenas tipo "longitud"

---
+++ aguilar/longitud.py
@@ -0,0 +1,120 @@
+print("="*70)
+#ejercicio 01 - longitud
+#Hallar la longitud de la frase
+frase1="LA IGUALDAD DE LA RIQUEZA DEBE CONSISTIR EN QUE NINGUN CIUDADANO SEA OPULENTO QUE PUEDA COMPRAR A OTRO Y NINGUNO TAN POBRE QUE VEA LA NECESIDAD DE VENDERSE"
+longitud_frase1=len(frase1)
+print("el numero de digitos que hay en la primera frase son: ", longitud_frase1)
+#fin_primer_ejercicio
+print("="*70)
+
+#ejercicio 02 - longitud
+#Hallar la longitud de la frase
+frase2="la vida debe ser comprendida desde atras pero debe ser vivida hacia adelante"
+longitud_frase2=len(frase2)
+print("el numero de digitos que hay en la segunda frase son: ", longitud_frase2)
+#fin_segundo_ejercicio
+print("="*70)
+
+#ejercicio 03 - longitud
+#Hallar la longitud de la frase
+frase3="el destino es lo qeu baraja las cartas, pero nosotros lo que la jugamos"
+longitud_frase3=len(frase3)
+print("el numero de digitos que hay en la tercera frase son: ", longitud_frase3)
+#fin_tercer_ejercicio
+print("="*70)
+
+#ejercicio 04 - longitud
+#Hallar la longitud de la frase
+frase4="EXISTEN DOS MANERAS DE SER FELIZ EN ESTA VIDA, UNA ES HACERCE EL IDIOTA Y LA SERLO"
+longitud_frase4=len(frase4)
+print("el numero de digitos que hay en la cuarta frase son: ", longitud_frase4)
+#fin_cuarto_ejercicio
+print("="*70)
+
+#ejercicio 05 - longitud
+#Hallar la longitud de la frase
+frase5="SIEMPRE QUE ENSEÑES; ENSEÑA TAMBIEN, A LA VEZ, A DUDAR DE LO QUE ENSEÑAS"
+longitud_frase5=len(frase5)
+print("el numero de digitos que hay en la quinta frase son: ", longitud_frase5)
+#fin_quinto_ejercicio
+print("="*70)
+
+#ejercicio 06 - longitud
+#Hallar la longitud de la frase
+frase6="lo que importa no son los años de vida, sino la vida de los años"
+longitud_frase6=len(frase6)
+print("el numero de digitos que hay en la sexta frase son: ", longitud_frase6)
+#fin_SEXTO_ejercicio
+print("="*70)
+
+#ejercicio 07 - longitud
+#Hallar la longitud de la frase
+frase7="HACE FALTA TODA UNA VIDA PARA APRENDER A VIVIR"
+longitud_frase7=len(frase7)
+print("el numero de digitos que hay en la septima frase son: ", longitud_frase7)
+#fin_septimo_ejercicio
+print("="*70)
+
+#ejercicio 08 - longitud
+#Hallar la longitud de la frase
+frase8="SIN MUSICA LA VIDA SERIA UN ERROR"
+longitud_frase8=len(frase8)
+print("el numero de digitos que hay en la octaba frase son: ", longitud_frase8)
+#fin_octavo_ejercicio
+print("="*70)
+
+#ejercicio 09 - longitud
+#Hallar la longitud de la frase
+frase9="SOY MI PROPIO LIBRO. ME REESCRIBO, ME SUBRAYO, ME AGREGO PAGINAS, ME ARRANCO OTRAS QUE DUELEN. Y DEJO EN BLANCO UNA ULTIMA HOJA, SIEMPRE "
+longitud_frase9=len(frase9)
+print("el numero de digitos que hay en la novena frase son: ", longitud_frase9)
+#fin_noveno_ejercicio
+print("="*70)
+
+#ejercicio 10 - longitud
+#Hallar la longitud de la frase
+frase10="El sabio puede cambiar de opinio, el necio nunca"
+longitud_frase10=len(frase10)
+print("el numero de digitos que hay en la decima frase son: ", longitud_frase10)
+#fin_decimo_ejercicio
+print("="*70)
+
+#ejercicio 11 - longitud
+#Hallar la longitud de la frase
+frase11="El hombre es la unica criatura que rechaza ser lo que es"
+longitud_frase11=len(frase5)
+print("el numero de digitos que hay en la decima primera frase son: ", longitud_frase11)
+#fin_decimo_primero_ejercicio
+print("="*70)
+
+#ejercicio 12 - longitud
+#Hallar la longitud de la frase
+frase12="Divide las dificultades que examines en tantas partes como sea posible para su mejor solucion"
+longitud_frase12=len(frase12)
+print("el numero de digitos que hay en la decima segunda frase son: ", longitud_frase12)
+#fin_decimo_segundo_ejercicio
+print("="*70)
+
+#ejercicio 13 - longitud
+#Hallar la longitud de la frase
+frase13="Todos somos muy ignorantes. Lo que ocurre es lo que no todos ignoramos las mismas cosas"
+longitud_frase13=len(frase13)
+print("el numero de digitos que hay en la decima tercera frase son: ", longitud_frase13)
+#fin_decimo_tercero_ejercicio
+print("="*70)
+
+#ejercicio 14 - longitud
+#Hallar la longitud de la frase
+frase14="Si el conocimiento puede crear problemas, no es como la ignorancia con lop que podremos resolverlos"
+longitud_frase14=len(frase14)
+print("el numero de digitos que hay en la decima cuarta frase son: ", longitud_frase14)
+#fin_decimo_cuarto _ejercicio
+print("="*70)
+
+#ejercicio 15 - longitud
+#Hallar la longitud de la frase
+frase15="El placer del amor dura solamente un momento, el dolor del amor dura toda la vida"
+longitud_frase15=len(frase15)
+print("el numero de digitos que hay en la decima quinta frase son: ", longitud_frase15)
+#fin_decimo_quinto_ejercicio
+print("="*70)
