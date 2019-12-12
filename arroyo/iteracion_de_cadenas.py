4c908f04a91c75ca5e3ff719f25e3d20eb60585c
miguel aguilar <maguilarv@unprg.edu.pe>
2019-12-11 20:04:03 -0500

    se agrego los 15 ejercicios sobre cadenas tipo "iteracion" de mi compañero

---
+++ arroyo/iteracion_de_cadenas.py
@@ -0,0 +1,118 @@
+#EJERCICIOS DE ITERACION
+#EJERCICIOS 01
+#ESTA CADENA ESTARA EXPRESADA  EN FORMA INDIVIDUAL y VERTICAL
+nombre="Gonzalo"
+for letra in nombre:
+    print(letra)
+#fin_iterador
+
+#EJERCICIOS 02
+#ESTA CADENA ESTARA EXPRESADA  EN FORMA INDIVIDUAL y VERTICAL
+nombre1="PEDRO MARMOL ES UN PERSONAJE DE UNA CARICATURA"
+for letra in nombre1:
+    print(letra)
+#fin_iterador
+
+#EJERCICIOS 03
+#ESTA CADENA ESTARA EXPRESADA  EN FORMA INDIVIDUAL y VERTICAL
+nombre="PEDRO RUIZ GALLO "
+for letra in nombre:
+    print(letra)
+#fin_iterador
+
+#EJERCICIOS 04
+#ESTA CADENA ESTARA EXPRESADA  EN FORMA INDIVIDUAL y VERTICAL
+nombre="Arroyo Bernilla"
+for letra in nombre:
+    print(letra)
+#fin_iterador
+
+#EJERCICIOS 05
+#ESTA CADENA ESTARA EXPRESADA  EN FORMA INDIVIDUAL y VERTICAL
+nombre="LAMBAYEQUE; departamento de PERU"
+for letra in nombre:
+    print(letra)
+#fin_iterador
+
+#EJERCICIOS 06
+#ESTA CADENA ESTARA EXPRESADA  EN FORMA INDIVIDUAL y VERTICAL
+nombre="Mi cortina es de color; BLANCO"
+for letra in nombre:
+    print(letra)
+#fin_iterador
+
+#EJERCICIOS 07
+#ESTA CADENA ESTARA EXPRESADA  EN FORMA INDIVIDUAL y VERTICAL
+nombre="Me gusta jugar futbol"
+for letra in nombre:
+    print(letra)
+#fin_iterador
+
+#EJERCICIOS 08
+#ESTA CADENA ESTARA EXPRESADA  EN FORMA INDIVIDUAL y VERTICAL
+nombre="LAs personas suelen dormir por la noche"
+for letra in nombre:
+    print(letra)
+#fin_iterador
+
+#EJERCICIOS 09
+#ESTA CADENA ESTARA EXPRESADA  EN FORMA INDIVIDUAL y VERTICAL
+nombre="Programacion es el curso que mas me gusta"
+for letra in nombre:
+    print(letra)
+#fin_iterador
+
+#EJERCICIOS 10
+#ESTA CADENA ESTARA EXPRESADA  EN FORMA INDIVIDUAL y VERTICAL
+nombre="+Cuando te vi+ es una e mis canciones preferidas"
+for letra in nombre:
+    print(letra)
+#fin_iterador
+
+#EJERCICIOS 11
+#ESTA CADENA ESTARA EXPRESADA  EN FORMA INDIVIDUAL y VERTICAL
+nombre=" Yo vivo en chiclayo"
+for letra in nombre:
+    print(letra)
+#fin_iterador
+
+#EJERCICIOS 12
+#ESTA CADENA ESTARA EXPRESADA  EN FORMA INDIVIDUAL y VERTICAL
+nombre="mi celular es de marca Huawei"
+for letra in nombre:
+    print(letra)
+#fin_iterador
+
+#EJERCICIOS 13
+#ESTA CADENA ESTARA EXPRESADA  EN FORMA INDIVIDUAL y VERTICAL
+nombre="Actualmente tengo 18 años"
+for letra in nombre:
+    print(letra)
+#fin_iterador
+
+#EJERCICIOS 14
+#ESTA CADENA ESTARA EXPRESADA  EN FORMA INDIVIDUAL y VERTICAL
+nombre="El mes de mi cumpleaños es en septiembre"
+for letra in nombre:
+    print(letra)
+#fin_iterador
+
+#EJERCICIOS 15
+#ESTA CADENA ESTARA EXPRESADA  EN FORMA INDIVIDUAL y VERTICAL
+nombre="Mi mejor amigo se llama JHON"
+for letra in nombre:
+    print(letra)
+#fin_iterador
+
+
+
+
+#MENSAJE INCRIPTADO
+#INPUT
+MSG="ABCDFE"
+#ouput
+print("COMUNICADO")
+print("SEGUN LA AVERIGUACIONES, LA NASA HA DEJADO EL SIGUIENTE MENSAJE:" , MSG)
+print("que significa: ")
+
+#DESIFRAR EL SIGUIENTE MENSAJE
