2f2932e56749627c4a5f71b47b84d28a116a79bf
miguel aguilar <maguilarv@unprg.edu.pe>
2019-12-11 19:38:05 -0500

    se agrego los 15 ejercicios sobre cadenas tipo concatenacion

---
+++ aguilar/concatenacion.py
@@ -0,0 +1,180 @@
+print("="*70)
+#ejercicio 1 - concatenacion
+#concatenar los mensajes 1, 2 y 3
+#Y formar el mensaje: todo cambio dentro de mi
+msg1="todo cambio"
+msg2="dentro de"
+msg3="mi"
+msgf=msg1+" "+ msg2+ " "+msg3
+print(msgf)
+#FIN_CONCATENACION
+print("="*70)
+
+#ejercicio 2 - concatenacion
+#concatenar los mensajes 4, 5, 6, 7, 8, 9, 10, 11
+#Y formar el mensaje: hay un ultimo dia para todas las cosas
+msg4="hay"
+msg5="un"
+msg6="ultimo"
+msg7="dia"
+msg8="para"
+msg9="todas"
+msg10="las"
+msg11="cosas"
+msgf=msg4+" "+ msg5+ " "+ msg6 + " " + msg7 + " " + msg8 + " " + msg9 + " " + msg10 + " " + msg11
+print(msgf)
+#FIN_CONCATENACION
+print("="*70)
+
+#ejercicio 3 - concatenacion
+#concatenar los mensajes 12, 13 y 14
+#Y formar el mensaje: PEDRO RUIZ FALLO
+msg12="pedro".upper()
+msg13="ruiz".upper()
+msg14="gallo".upper()
+msgf=msg12+" "+ msg13+ " "+msg14
+print(msgf)
+#FIN_CONCATENACION
+print("="*70)
+
+#ejercicio 4 - concatenacion
+#concatenar los mensajes 15 y 16
+#Y formar el mensaje: KARL WEISS
+msg15="KARL"
+msg16="WEISS"
+msgf=msg15+" "+ msg16
+print(msgf)
+#FIN_CONCATENACION
+print("="*70)
+
+#ejercicio 5 - concatenacion
+#concatenar los mensajes 17, 18, 19, 20, 21, 22
+#Y formar el mensaje: SOLO TE PIDO TRES DIAS MAS
+msg17="SOLO"
+msg18="TE"
+msg19="PIDO"
+msg20="TRES"
+msg21="DIAS"
+msg22="MAS"
+msgf=msg17+" "+ msg18 + " "+ msg19 + " " + msg20 + " " + msg21 + " " + msg22
+print(msgf)
+#FIN_CONCATENACION
+print("="*70)
+
+#ejercicio 6 - concatenacion
+#concatenar los mensajes 24, 25 y 26
+#Y formar el mensaje: CREO EN TI
+msg24="creo".upper()
+msg25="en".upper()
+msg26="ti".upper()
+msgf=msg24+" "+ msg25+ " "+msg26
+print(msgf)
+#FIN_CONCATENACION
+print("="*70)
+
+#ejercicio 7 - concatenacion
+#concatenar los mensajes 27, 28 y 29
+#Y formar el mensaje:HACE FALTA TODA UNA VIDA PARA APRENDER A VIVIR
+msg27="hace falta".upper()
+msg28="toda una vida ".upper()
+msg29="para aprender a vivir".upper()
+msgf=msg27+" "+ msg28+ " "+msg29
+print(msgf)
+#FIN_CONCATENACION
+print("="*70)
+
+#ejercicio 8 - concatenacion
+#concatenar los mensajes 30, 31 y 32
+#y formar el mensaje: si el enemigo se equivoca no lo distraigas
+msg30="si el"
+msg31="enemigo se equivoca"
+msg32="no lo distraigas"
+msgf=msg30+" "+ msg31+ " "+msg32
+print(msgf)
+#FIN_CONCATENACION
+print("="*70)
+
+#ejercicio 9 - concatenacion
+#concatenar los mensajes 33, 34, 35, 36 Y 37
+#Y foormar el mensaje: El hombre es la unica criatura que rechaza 'SER LO QUE ES'
+msg33="El hombre"
+msg34="es la unica"
+msg35="criatura que rechaza"
+msg36="ser".upper()
+msg37="lo que es".upper()
+msgf=msg33+" "+ msg34+ " "+msg35+" '"+msg36+" "+ msg37 + "'"
+print(msgf)
+#FIN_CONCATENACION
+print("="*70)
+
+#ejercicio 10 - concatenacion
+#concatenar los mensajes 38, 39 y 40
+msg38="Academia"
+msg39="preuniversitaria"
+msg40="gajel".upper()
+msgf=msg38+" "+ msg39+ " '"+msg40+"'"
+print(msgf)
+#FIN_CONCATENACION
+print("="*70)
+
+#ejercicio 11 - concatenacion
+#concatenar los mensajes 41, 42, 43 y 44
+#Y formar el mensaje: La obra maestra de la injusticia es parecer justo 'SIN SERLO'
+msg41="La obra maestra"
+msg42="de la injusticia"
+msg43="es parecer justo"
+msg44="sin serlo".upper()
+msgf=msg41+" "+ msg42+ " "+msg43+" '"+msg44+"'"
+print(msgf)
+#FIN_CONCATENACION
+print("="*70)
+
+#ejercicio 12 - concatenacion
+#concatenar los mensajes 45, 46, 47 Y 48
+#Y formar el mensaje: El destino es el que baraja las cartas, pero nosotros los que la jugamos
+msg45="El destino es"
+msg46="el que baraja las cartas"
+msg47="pero nosotros los que"
+msg48="la jugamos"
+msgf=msg45+" "+ msg46+ ", "+msg47+" "+msg48
+print(msgf)
+#FIN_CONCATENACION
+print("="*70)
+
+#ejercicio 13 - concatenacion
+#concatenar los mensajes 49, 50, 51 y 52
+#y formar el mensaje: Los hombres construimos demasiados muros y no suficientes PUENTES
+msg49="Los hombres"
+msg50="construimos demasiados"
+msg51="muros y no"
+msg52="suficientes puentes"
+cor1=msg52[0:11]
+cor2=msg52[12:].upper()
+msgf=msg49+" "+ msg50+ " "+msg51+" "+cor1+" "+cor2
+print(msgf)
+#FIN_CONCATENACION
+print("="*70)
+
+#ejercicio 14 - concatenacion
+#concatenar los mensajes 53, 54 y 55
+#Y formar el mensaje: la verdadera sabiduria esta enh reconocer la propia IGNORANCIA
+msg53="la verdadera sabiduria"
+msg54="esta en reconocer la propia"
+msg55="ignoracia".upper()
+msgf=msg53+" "+ msg54+ " "+msg55
+print(msgf)
+#FIN_CONCATENACION
+print("="*70)
+
+#ejercicio 15 - concatenacion
+#concatenar los mensajes 56, 57 y 58
+#Y formar el mensaje: El que juzga encontrara la vida facil
+msg56="El que todo"
+msg57="juzga"
+msg58="encontrara"
+msg59="la vida"
+msg60="FACIL"
+msgf=msg56+" "+ msg57+ " "+msg58+ " " + msg59 + " " + msg60
+print(msgf)
+#FIN_CONCATENACION
+print("="*70)
