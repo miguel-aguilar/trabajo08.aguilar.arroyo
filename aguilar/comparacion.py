6e61847170da964ccc6d245b54822fbab43b5107
miguel aguilar <maguilarv@unprg.edu.pe>
2019-12-11 19:36:00 -0500

    se agrego los 15 ejercicios sobre cadenas tipo comparacion

---
+++ aguilar/comparacion.py
@@ -0,0 +1,150 @@
+print("="*70)
+#ejercicio 01 - comparacion
+#comparando la cadena 1 y 2
+cadena1="ALACRAN"
+cadena2="alerta"
+#comparador
+comparador=(cadena1!=cadena2)
+print("¿la cadena 1 es diferente a la cadena 2?:", comparador)
+#fin_comparador
+print("="*70)
+
+#ejercicio 02 - comparacion
+#comparando la cadena 3 y 4
+cadena3="esconder".upper()
+cadena4="ESCONDER"
+#comparador
+comparador=(cadena3==cadena4)
+print("¿la cadena 3 es igual a la cadena 4?: ", comparador)
+#fin_comparador
+print("="*70)
+
+#ejercicio 03 - comparacion
+#comparando la cadena 5 y 6
+cadena5="cada parsona escoje como ser feliz"
+cadena6="cada persona escoge como ser feliz".upper()
+#comparador
+comparador=(cadena5<cadena6)
+print("¿la cadena 5 es menor a la cadena 6?: ", comparador)
+#fin_comparador
+print("="*70)
+
+#ejercicio 04 - comparacion
+#comparando la cadena 7 y 8
+cadena7="el gran caballero andante"
+cadena8="EL GRAN CABALLERO ".lower()
+#comparador
+comparador=(cadena7>cadena8)
+print("¿la cadena 7 es mayor a la cadena 8?: ", comparador)
+#fin_comparador
+print("="*70)
+
+#ejercicio 05 - comparacion
+#comparando la cadena 9 y 10
+cadena9="el que calla otorga"
+cadena10="EL QUE HABLA OTORGA".lower()
+#comparador
+comparador=(cadena9==cadena10)
+print("¿la cadena 9 es igual a la cadena 10?: ", comparador)
+#fin_comparador
+print("="*70)
+
+#ejercicio 06 - comparacion
+#comparando la cadena 11 y 12
+cadena11="el que sigue la consigue"
+cadena12="EL QUE SIGUE LA CONSIGUE".lower()
+#comparador
+comparador=(cadena11==cadena12)
+print("¿las cadenas 11 y 12 son iguales?: ", comparador)
+#fin_comparador
+print("="*70)
+
+#ejercicio 07 - comparacion
+#comparando la cadena 13 y 14
+cadena13="LA SONRISA ES MIA PERO EL MOTIVO ERES TU"
+cadena14="en cada caida esta obligatorio levantarse ".upper()
+#comparador
+comparador=(cadena14<cadena13)
+print("¿las cadena 14 es menor a la cadena 13?: ", comparador)
+#fin_comparador
+print("="*70)
+
+#ejercicio 08 - comparacion
+#comparando la cadena 15 y 16
+cadena15="la vida es muy simple pero insistimos en hacerla complicada"
+cadena16="LA VIDA ES MUY SIMPLE PERO INSISTIMOS EN HACERLA COMPLICADA".lower()
+#comparador
+comparador=(cadena15!=cadena16)
+print("¿la cadena 15 es diferente a la cadena 16?: ", comparador)
+#fin_comparador
+print("="*70)
+
+#ejercicio 09 - comparacion
+#comparando la cadena 17 y 18
+cadena17="primero piensa, luego razona y por ultimo actua"
+cadena18="la felicidad no es un ideal de la razon sino de la imaginacion"
+#comparador
+comparador=(cadena17>=cadena18)
+print("¿la cadena 17 es mayor o igual a la cadena 18?: ", comparador)
+#fin_comparador
+print("="*70)
+
+#ejercicio 10 - comparacion
+#comparando la cadena 19 y 20
+cadena19="la vida es como un libro sin indice"
+cadena20="la vida es como un libro sin indice".upper()
+#comparador
+comparador=(cadena19!=cadena20)
+print("¿la cadena 19 es diferente a la cadena 20?: ", comparador)
+#fin_comparador
+print("="*70)
+
+#ejercicio 11 - comparacion
+#comparando la cadena 21 y 22
+cadena21="te fuiste de aqui y encontraste otra vida"
+cadena22="TE FUISTE DE AQUI Y ENCONTRASTE OTRA VIDA".lower()
+#comparador
+comparador=(cadena21==cadena22)
+print("¿la cadena 21 es igual a la cadena 22?: ", comparador)
+#fin_comparador
+print("="*70)
+
+#ejercicio 12 - comparacion
+#comparando la cadena 23 y 24
+cadena23="hay un ultimo dia para todas las cosas"
+cadena24="HAY UN ULTIMO DIA PARA TODAS LAS C0SAS".lower()
+#comparador
+comparador=(cadena23==cadena24)
+print("¿la cadena 23 es igual a la cadena 24?: ", comparador)
+#fin_comparador
+print("="*70)
+
+#ejercicio 13 - comparacion
+#comparando la cadena 25 y 26
+cadena25="tenemos que conseguir el licenciamiento"
+cadena26="el licenciamiento debemos conseguirlo"
+#comparador
+comparador=(cadena25!=cadena26)
+print("¿la cadena 25 es diferente a la cadena 26?: ", comparador)
+#fin_comparador
+print("="*70)
+
+#ejercicio 14 - comparacion
+#comparando la cadena 27 y 28
+cadena27="si el rio suena es porque piedras trae"
+cadena28="SI EL RIO SUENA ES PORQUE PIEDRAS TRAE".lower()
+#comparador
+comparador=(cadena27==cadena28)
+print("¿la cadena 27 es igual a la cadena 28?: ", comparador)
+#fin_comparador
+print("="*70)
+
+#ejercicio 15 - comparacion
+#comparando la cadena 29 y 30
+cadena29="a caballo regalado, no se le mira el diente"
+cadena30="camaron que se duerme se lo lleva la corriente".upper()
+#comparador
+comparador=(cadena29<=cadena30)
+print("¿la cadena 29 es menor o igual a la cadena 30?: ", comparador)
+#fin_comparador
+print("="*70)
