230ac691eb81de3f402bbab54cf4c62374fe6b99
miguel-aguilar <57012427+miguel-aguilar@users.noreply.github.com>
2019-12-11 19:33:30 -0500

    se agrego los 15 ejercicios sobre cadenas tipo indexacion

---
+++ aguilar/indexacion.py
@@ -0,0 +1,255 @@
+print("="*25)
+#ejercicio 01 - indexacion
+#IMPRIMIR LA PALABRA: ' B O D A '
+#              10        20
+#    0123456789012345678901234
+msg="MI LIBRO 'LUNA DE PLUTON'"
+#INDEXACION
+sig1=msg[9]  #  devuelve  -> '
+letraB=msg[5]  #  devuelve  -> B
+letraO=msg[7]  #  devuelve  -> O
+letraD=msg[15]  #  devuelve  -> D
+letraA=msg[13]  #  devuelve  -> A
+sig2=msg[24]  #  devuelve  -> '
+print(sig1,letraB,letraO,letraD,letraA,sig2)
+#FIN_PRIMER_EJERCICIO
+print("="*25)
+
+#ejercicio 02 - indexacion
+#IMPRIMIR LA PALABRA: S A N O
+#                10        20
+#      012345678901234567890123456
+letra="SE HACE LA QUE NO ME CONOCE"
+#INDEXACION
+pa1=letra[0]  #  devuelve  -> S
+pa2=letra[4]  #  devuelve  -> A
+pa3=letra[15]  #  devuelve  -> N
+pa4=letra[16]  #  devuelve  -> O
+print(pa1,pa2,pa3,pa4)
+#FIN_SEGUNDO_EJERCICIO
+print("="*25)
+
+#ejercicio 03 - indexacion
+#IMPRIMIR LA PALABRA: T O R R E
+#                      10        20        30        40
+#            01234567890123456789012345678901234567890123456
+trabalengua="TRES TRISTES TIGRES TRAGARON TRIGO EN UN TRIGAL"
+#INDEXACION
+tra1=trabalengua[0]  #  devuelve  -> T
+tra2=trabalengua[26]  #  devuelve  -> O
+tra3=trabalengua[6]  #  devuelve  -> R
+tra4=trabalengua[1]  #  devuelve  -> R
+tra5=trabalengua[2]  #  devuelve  -> E
+print(tra1,tra2,tra3,tra4,tra5)
+#FIN_TERCER_EJERCICIO
+print("="*25)
+
+#ejercicio 04 - indexacion
+#IMPRIMIR LA PALABRA: p a s e a r
+#                10        20        30
+#      01234567890123456789012345678901234
+frase="YA LO PASADO PASADO, NO ME INTERESA"
+#INDEXACION
+fra1=frase[6].lower()  #  devuelve  -> p
+fra2=frase[7].lower()  #  devuelve  -> a
+fra3=frase[8].lower()  #  devuelve  -> s
+fra4=frase[25].lower()  #  devuelve  -> e
+fra5=frase[34].lower()  #  devuelve  -> a
+fra6=frase[31].lower()  #  devuelve  -> r
+print(fra1,fra2,fra3,fra4,fra5,fra6)
+#FIN_CUARTO_EJERCICIO
+print("="*25)
+
+#ejercicio 05 - indexacion
+#IMPRIMIR LA PALABRA: S E N T I R
+#                10        20        30        40        50
+#      01234567890123456789012345678901234567890123456789012345678
+frase="la vida es muy simple pero insistimos en hacerla complicada"
+#INDEXACION
+fra1=frase[15].upper()  #  devuelve  -> S
+fra2=frase[8].upper()  #  devuelve  -> E
+fra3=frase[28].upper()  #  devuelve  -> N
+fra4=frase[32].upper()  #  devuelve  -> T
+fra5=frase[54].upper()  #  devuelve  -> I
+fra6=frase[24].upper()  #  devuelve  -> R
+print(fra1,fra2,fra3,fra4,fra5,fra6)
+#FIN_QUINTO_EJERCICIO
+print("="*25)
+
+#ejercicio 06 - indexacion
+#IMPRIMIR LA PALABRA: N A V I D A D
+#                10        20        30        40        50        60        70        80        90
+#      0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123
+frase="Nunca discutas con un estupido, te hara descender a su nivel  y ahi te vencera por experiencia"
+#INDEXACION
+fra1=frase[0] #  devuelve  -> N
+fra2=frase[4].upper()  #  devuelve  -> A
+fra3=frase[57].upper()  #  devuelve  -> V
+fra4=frase[7].upper()  #  devuelve  -> I
+fra5=frase[6].upper()  #  devuelve  -> D
+fra6=frase[12].upper()  #  devuelve  -> A
+fra7=frase[28].upper()  #  devuelve  -> D
+print(fra1,fra2,fra3,fra4,fra5,fra6,fra7)
+#FIN_SEXTO_EJERCICIO
+print("="*25)
+
+#ejercicio 07 - indexacion
+#IMPRIMIR LA PALABRA: P L A T O N
+#                10        20        30        40        50
+#      01234567890123456789012345678901234567890123456789012345678
+frase="LA OBRA MAESTRA DE LA INJUSTICIA ES PARECER JUSTO SIN SERLO"
+#INDEXACION
+fra1=frase[36] #  devuelve  -> P
+fra2=frase[0] #  devuelve  -> L
+fra3=frase[1]  #  devuelve  -> A
+fra4=frase[27]  #  devuelve  -> T
+fra5=frase[48]  #  devuelve  -> O
+fra6=frase[52]  #  devuelve  -> N
+print(fra1,fra2,fra3,fra4,fra5,fra6)
+#FIN_SEPTIMO_EJERCICIO
+print("="*25)
+
+#ejercicio 08 - indexacion
+#IMPRIMIR LA PALABRA: L E A L T A D
+#                10        20        30        40        50
+#      0123456789012345678901234567890123456789012345678901234567
+frase="QUIEN REVISA LO QUE NO DEBE, SE ENTERA DE LO QUE NO QUIERE"
+#INDEXACION
+fra1=frase[42] #  devuelve  -> L
+fra2=frase[40]  #  devuelve  -> E
+fra3=frase[11]  #  devuelve  -> A
+fra4=frase[13]  #  devuelve  -> L
+fra5=frase[34] #  devuelve  -> T
+fra6=frase[37]  #  devuelve  -> A
+fra7=frase[39]  #  devuelve  -> D
+print(fra1,fra2,fra3,fra4,fra5,fra6,fra7)
+#FIN_OCTAVO_EJERCICIO
+print("="*25)
+
+#ejercicio 09 - indexacion
+#IMPRIMIR LA PALABRA: E I N S T E I N
+#                10        20        30        40        50        60        70        80        90
+#      0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456
+frase="hay una fueza motriz mas poderosa que el vapor, la electricidad y la energia atomica: la voluntad"
+#INDEXACION
+fra1=frase[10].upper()  #  devuelve  -> E
+fra2=frase[18].upper()  #  devuelve  -> I
+fra3=frase[70].upper()  #  devuelve  -> N
+fra4=frase[31].upper()  #  devuelve  -> S
+fra5=frase[16].upper()  #  devuelve  -> T
+fra6=frase[28].upper()  #  devuelve  -> E
+fra7=frase[57].upper()  #  devuelve  -> I
+fra8=frase[93].upper()  #  devuelve  -> N
+print(fra1,fra2,fra3,fra4,fra5,fra6,fra7,fra8)
+#FIN_NOVENO_EJERCICIO
+print("="*25)
+
+#ejercicio 10 - indexacion
+#IMPRIMIR LA PALABRA: M A R L O N
+#                10        20        30        40        50        60        70
+#      01234567890123456789012345678901234567890123456789012345678901234567890123
+frase="NADA SE OLVIDA MAS DESPACIO QUE UNA OFENSA Y NADA, MAS RAPIDO QUE UN FAVOR"
+#INDEXACION
+fra1=frase[15].lower() #  devuelve  -> M
+fra2=frase[1].lower()  #  devuelve  -> A
+fra3=frase[55].lower()  #  devuelve  -> R
+fra4=frase[9].lower()  #  devuelve  -> L
+fra5=frase[8].lower()  #  devuelve  -> O
+fra6=frase[67].lower()  #  devuelve  -> N
+
+print(fra1,fra2,fra3,fra4,fra5,fra6)
+#FIN_DECIMO_EJERCICIO
+print("="*25)
+
+#ejercicio 11 - indexacion
+#IMPRIMIR LA PALABRA: I M M A N U E L
+#                10        20        30        40        50        60
+#      01234567890123456789012345678901234567890123456789012345678901
+frase="la felicidad no es un ideal de la razon sino de la imaginacion"
+#INDEXACION
+fra1=frase[6].upper() #  devuelve  -> I
+fra2=frase[52].upper()  #  devuelve  -> M
+fra3=frase[52].upper()  #  devuelve  -> M
+fra4=frase[10].upper()  #  devuelve  -> A
+fra5=frase[13].upper()  #  devuelve  -> N
+fra6=frase[19].upper()  #  devuelve  -> U
+fra7=frase[24].upper()  #  devuelve  -> E
+fra8=frase[5].upper()  #  devuelve  -> NL
+print(fra1,fra2,fra3,fra4,fra5,fra6,fra7,fra8)
+#FIN_DECIMO_PRIMERO_EJERCICIO
+print("="*25)
+
+#ejercicio 12 - indexacion
+#IMPRIMIR LA PALABRA: V I V I R L O
+#                10        20        30        40        50        60        70
+#      012345678901234567890123456789012345678901234567890123456789012345678901234
+frase="LA VIDA NO ES UN PROBLEMA PARA SER RESUELTO, ES UN MISTERIO PARA SER VIVIDO"
+#INDEXACION
+fra1=frase[3] #  devuelve  -> V
+fra2=frase[52]  #  devuelve  -> I
+fra3=frase[69]  #  devuelve  -> V
+fra4=frase[4]  #  devuelve  -> I
+fra5=frase[18]  #  devuelve  -> R
+fra6=frase[21]  #  devuelve  -> L
+fra7=frase[9]  #  devuelve  -> O
+print(fra1,fra2,fra3,fra4,fra5,fra6,fra7)
+#FIN_DECIMO_SEGUNDO_EJERCICIO
+print("="*25)
+
+#ejercicio 13 - indexacion
+#IMPRIMIR LA PALABRA: I N T E L I G E N T E
+#                10        20        30        40
+#      0123456789012345678901234567890123456789012
+frase="si el enemigo se equivoca, no lo distraigas"
+#INDEXACION
+fra1=frase[1].upper()  #  devuelve  -> I
+fra2=frase[7].upper()  #  devuelve  -> N
+fra3=frase[36].upper()  #  devuelve  -> T
+fra4=frase[15].upper()  #  devuelve  -> E
+fra5=frase[30].upper()  #  devuelve  -> L
+fra6=frase[10].upper()  #  devuelve  -> I
+fra7=frase[11].upper()  #  devuelve  -> G
+fra8=frase[8].upper()  #  devuelve  -> E
+fra9=frase[7].upper()  #  devuelve  -> N
+fra10=frase[36].upper()  #  devuelve  -> T
+fra11=frase[17].upper()  #  devuelve  -> E
+print(fra1,fra2,fra3,fra4,fra5,fra6,fra7,fra8,fra9,fra10,fra11)
+#FIN_DECIMO_TERCERO_EJERCICIO
+print("="*25)
+
+#ejercicio 14 - indexacion
+#IMPRIMIR LA PALABRA: A U T O E S T I M A
+#                10        20        30        40        50
+#      01234567890123456789012345678901234567890123456789012345
+frase="el hombre es la unica criatura que rechaza ser lo que es"
+#INDEXACION
+fra1=frase[14].upper()  #  devuelve  -> A
+fra2=frase[16].upper()  #  devuelve  -> U
+fra3=frase[26].upper()  #  devuelve  -> T
+fra4=frase[48].upper()  #  devuelve  -> O
+fra5=frase[8].upper()  #  devuelve  -> E
+fra6=frase[11].upper()  #  devuelve  -> S
+fra7=frase[26].upper()  #  devuelve  -> T
+fra8=frase[24].upper()  #  devuelve  -> I
+fra9=frase[5].upper()  #  devuelve  -> M
+fra10=frase[41].upper()  #  devuelve  -> A
+print(fra1,fra2,fra3,fra4,fra5,fra6,fra7,fra8,fra9,fra10)
+#FIN_DECIMO_CUARTO_EJERCICIO
+print("="*25)
+
+#ejercicio 15 - indexacion
+#IMPRIMIR LA PALABRA: s u p e r a r
+#                10        20        30        40        50        60
+#      01234567890123456789012345678901234567890123456789012345678901234
+frase="LOS HOMBRES CONSTRUIMOS DEMASIADOS MUROS Y NO SUFICIENTES PUENTES"
+#INDEXACION
+fra1=frase[10].lower()  #  devuelve  -> s
+fra2=frase[18].lower()  #  devuelve  -> u
+fra3=frase[58].lower()  #  devuelve  -> p
+fra4=frase[60].lower()  #  devuelve  -> e
+fra5=frase[8].lower()  #  devuelve  -> r
+fra6=frase[30].lower()  #  devuelve  -> a
+fra7=frase[37].lower()  #  devuelve  -> r
+print(fra1,fra2,fra3,fra4,fra5,fra6,fra7)
+#FIN_DECIMO_QUINTO_EJERCICIO
+print("="*25)
