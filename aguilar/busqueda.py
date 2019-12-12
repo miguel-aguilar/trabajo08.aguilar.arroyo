61382078848fca5107ac549cc5c7e6af47d3eae4
miguel aguilar <maguilarv@unprg.edu.pe>
2019-12-11 19:42:00 -0500

    se agrego los 15 ejercicios sobre cadenas tipo "busqueda"

---
+++ aguilar/busqueda.py
@@ -0,0 +1,140 @@
+print("="*50)
+#ejercicio 1 = buscador
+#buscar la palabra: MENUDO
+frase1="EL QUE TIENE GRANDES PENSAMIENTOS, A MENUDO COMETE GRANDES ERRORES"
+buscador1=frase1.index("MENUDO")
+print("La palabra MENUDO esta en la posicion: ", buscador1)
+print("="*50)
+#FIN_BUSCADOR
+
+#ejercicio 2 = buscador
+#buscar la palabra: ENSEÑA
+frase2="SIEMPRE QUE ENSEÑES; ENSEÑA TAMBIEN, A LA VES, A DUDAR DE LO QUE ENSEÑAS"
+buscador2=frase2.index("ENSEÑA")
+print("La palabra ENSEÑA esta en la posicion: ", buscador2)
+print("="*50)
+#FIN_BUSCADOR
+
+#ejercicio 3 = buscador
+#buscar la palabra: DESCENDER
+frase3="NUNCA DISCUTAS CON UN ESTUPIDO,TE HARA DESCENDER A SU NIVEL Y AHI TE VENCERA POR EXPERIANCIA"
+buscador3=frase3.index("DESCENDER")
+print("La palabra DESCEDNDER esta en la posicion: ", buscador3)
+print("="*50)
+#FIN_BUSCADOR
+
+#ejercicio 4 = buscador
+#buscar la palabra: CUESTIONAR
+frase4="NUNCA FUI CONSCIENTE DE CUALQUIER OTRA OPCION QUE NO FUERA LA DE CUESTIONAR TODO"
+buscador4=frase4.index("CUESTIONAR")
+print("La palabra CUESTIONAR esta en la posicion: ", buscador4)
+print("="*50)
+#FIN_BUSCADOR
+
+#ejercicio 5 = buscador
+#buscar la palabra: IDIOTA
+frase5="EXISTEN DOS MANERAS DE SER FELIZ EN ESTA VIDA, UNA ES HACERSE EL IDIOTA Y LA OTRA ES SERLO"
+buscador5=frase5.index("IDIOTA")
+print("La palabra IDIOTA esta en la posicion: ", buscador5)
+print("="*50)
+#FIN_BUSCADOR
+
+#ejercicio 6 = buscador
+#buscar la palabra: NECIO
+frase6="EL SABIO PUEDE CAMBIAR DE OPINION, EL NECIO NUNCA"
+buscador6=frase6.index("NECIO")
+print("La palabra NECIO esta en la posicion: ", buscador6)
+print("="*50)
+#FIN_BUSCADOR
+
+#ejercicio 7 = buscador
+#buscar la palabra: CURA
+frase7="EL AMOR NO TIENE CURA, PERO ES LA CURA PARA TODOS LOS MALES"
+buscador7=frase7.index("CURA")
+print("La palabra CURA esta en la posicion: ", buscador7)
+print("="*50)
+#FIN_BUSCADOR
+
+#ejercicio 8 = buscador
+#buscar la palabra: POBRE
+frase8="NO SE ES POBRE POR LO QUE TIENE EN EL BOLSILLO"
+buscador8=frase8.index("POBRE")
+print("La palabra POBRE esta en la posicion: ", buscador8)
+print("="*50)
+#FIN_BUSCADOR
+
+#ejercicio 9 = buscador
+#buscar la palabra:  APRENDER
+frase9="HACE FALTA TODA UNA VIDA PARA APRENDER A VIVIR"
+buscador9=frase9.index("APRENDER")
+print("La palabra APRENDER esta en la posicion: ", buscador9)
+print("="*50)
+#FIN_BUSCADOR
+
+#ejercicio 10 = buscador
+#buscar la palabra: ENEMIGO
+frase10="SI EL ENEMIGO SE EQUIVOCA, NO LO DISTRAIGAS"
+buscador10=frase10.index("ENEMIGO")
+print("La palabra ENEMIGO esta en la posicion: ", buscador10)
+print("="*50)
+#FIN_BUSCADOR
+
+#ejercicio 11 = buscador
+#buscar la palabras: CRIATURA y RECHAZA
+#luego sumar sus posiciones
+frase11="EL HOMBRE ES LA UNICA CRIATURA QUE RECHAZA SER LO QUE ES"
+buscador11a=frase11.index("CRIATURA")
+buscador11b=frase11.index("RECHAZA")
+#OPERACION
+suma1=buscador11a+buscador11b
+print("La suma de las posiciones es= ", suma1)
+print("="*50)
+#FIN_BUSCADOR
+
+#ejercicio 12 = buscador
+#buscar la palabras: SERIA y MUSICA
+#luego restar sus posiciones
+frase12="SIN MUSICA LA VIDA SERIA UN ERROR"
+buscador12a=frase12.index("SERIA")
+buscador12b=frase12.index("MUSICA")
+#OPERACION
+resta1=buscador12a=buscador12b
+print("La resta de las posiciones es= ", resta1)
+print("="*50)
+#FIN_BUSCADOR
+
+#ejercicio 13 = buscador
+#buscar la palabras: ULTIMO y COSAS
+#luego multiplicar sus posiciones
+frase13="HAY UN ULTIMO DIA PARA TODAS LAS COSAS"
+buscador13a=frase13.index("ULTIMO")
+buscador13b=frase13.find("CAR")
+#OPERACION
+producto=buscador13a*buscador13b
+print("El producto de sus posiciones es= ", producto)
+print("="*50)
+#FIN_BUSCADOR
+
+#ejercicio 14 = buscador
+#buscar la palabras: IGNORANCIA y CONOCIMIENTO
+#luego dividir sus posiciones
+frase14="SI EL CONOCIMIENTO PUEDE CREAR PROBLEMAS, NO ES CON LA IGNORANCIA CON LA QUE PODREMOS RESOLVERLOS"
+buscador14a=frase14.index("IGNORANCIA")
+buscador14b=frase14.index("CONOCIMIENTO")
+#OPERACION
+division=buscador11a/buscador11b
+print("La division de las posiciones es= ", division)
+print("="*50)
+#FIN_BUSCADOR
+
+#ejercicio 15 = buscador
+#buscar la palabras: MITAD y TODO
+#luego sumar sus posiciones
+frase15="DARIA TODO LO QUE SE POR LA MITAD DE LO QUE IGNORO"
+buscador15a=frase15.index("MITAD")
+buscador15b=frase15.index("TODO")
+#OPERACION
+suma=buscador15a+buscador15b
+print("La suma de las posiciones es= ", suma)
+print("="*50)
+#FIN_BUSCADOR
