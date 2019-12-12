58f6d3fcdb798261f8ff8eb162640c47e9f86990
miguel aguilar <maguilarv@unprg.edu.pe>
2019-12-11 20:00:01 -0500

    se agrego los 15 ejercicios sobre cadenas tipo "comparacion" de mi compañero

---
+++ arroyo/comparacion_de_cadenas.py
@@ -0,0 +1,121 @@
+#EJERCICIOS DE COMPARACION
+
+#EJERCICIO 1
+#COMPARACION DE LA cad1 y cad2
+cad1="las personas son buenas"
+cad2="LAS PERSONAS SON BUENAS"
+comparador=(cad1==cad2)
+print("el valor de la comparacion de ambas cadenas es: ", comparador)
+#fin_comparador
+
+#EJERCICIO 2
+#COMPARACION DE LA cad3 y cad4
+cad3="LA LEY DE LA GRAVEDAD"
+cad4="las leyes de la gravedad, fue dada por isacc newton"
+comparador=(cad3<cad4)
+print("el valor de la comparacion de ambas cadenas es: ", comparador)
+#fin_comparador
+
+#EJERCICIO 3
+#COMPARACION DE LA cad5 y cad6
+cad5="Universidad pedro Ruiz Gallo"
+cad6="Sunedu esta licenciando universidades"
+comparador=(cad5>cad6)
+print("el valor de la comparacion de ambas cadenas es: ", comparador)
+#fin_comparador
+
+#EJERCICIO 4
+#COMPARACION DE LA cad7 y cad8
+cad7="GONZALO ARROYO BERNILLA"
+cad8="GONZALO ARROYO BERNILLLA"
+comparador=(cad7!=cad8)
+print("el valor de la comparacion de ambas cadenas es: ", comparador)
+#fin_comparador
+
+#EJERCICIO 5
+#COMPARACION DE LA cad9 y cad10
+cad9="La marca de mi celular es huawei"
+cad10="La marca de mi celular es samsung"
+comparador=(cad9==cad10)
+print("el valor de la comparacion de ambas cadenas es: ", comparador)
+#fin_comparador
+
+#EJERCICIO 6
+#COMPARACION DE LA cad11 y cad12
+cad11="Mi curso favorito es programacion"
+cad12="El curso que no me gusta es tecnicas de estudio"
+comparador=(cad11==cad12)
+print("el valor de la comparacion de ambas cadenas es: ", comparador)
+#fin_comparador
+
+#EJERCICIO 7
+#COMPARACION DE LA cad13 y cad14
+cad13="Tumbes es el departamento mas pequeño de peru"
+cad14="Lima es la capital de peru"
+comparador=(cad13>cad14)
+print("el valor de la comparacion de ambas cadenas es: ", comparador)
+#fin_comparador
+
+#EJERCICIO 8
+#COMPARACION DE LA cad15 y cad16
+cad15="Los miercoles salgo temprano de la universidad"
+cad16="los lunes son dias muy aburridos"
+comparador=(cad15<cad16)
+print("el valor de la comparacion de ambas cadenas es: ", comparador)
+#fin_comparador
+
+#EJERCICIO 9
+#COMPARACION DE LA cad17 y cad18
+cad17="La carrera que estudio es ingenieria electronica"
+cad18="Mi amigo estudia Ingenieria Mecanica"
+comparador=(cad17==cad18)
+print("el valor de la comparacion de ambas cadenas es: ", comparador)
+#fin_comparador
+
+#EJERCICIO 10
+#COMPARACION DE LA cad19 y cad20
+cad19="En estas semanas tengo examenes"
+cad20="Ojala salga aprobado en todos"
+comparador=(cad19!=cad20)
+print("el valor de la comparacion de ambas cadenas es: ", comparador)
+#fin_comparador
+
+#EJERCICIO 11
+#COMPARACION DE LA cad21 y cad22
+cad21="En el examen de programacion saque 08 de nota"
+cad22="En el proximo examen espero sacar 18"
+comparador=(cad21==cad22)
+print("el valor de la comparacion de ambas cadenas es: ", comparador)
+#fin_comparador
+
+#EJERCICIO 12
+#COMPARACION DE LA cad23 y cad24
+cad23="las personas son buenas"
+cad24="LAS PERSONAS SON BUENAS"
+comparador=(cad23==cad24)
+print("el valor de la comparacion de ambas cadenas es: ", comparador)
+#fin_comparador
+
+#EJERCICIO 13
+#COMPARACION DE LA cad25 y cad26
+cad25="Yo vivo en Ferreñafe"
+cad26="La mayoria de mis amigos viven en chiclayo"
+comparador=(cad25==cad26)
+print("el valor de la comparacion de ambas cadenas es: ", comparador)
+#fin_comparador
+
+#EJERCICIO 14
+#COMPARACION DE LA cad27 y cad28
+cad27="La universidad esta ubicada en lambayeque"
+cad28="Yo vivo muy lejos de la universidad"
+comparador=(cad27==cad28)
+print("el valor de la comparacion de ambas cadenas es: ", comparador)
+#fin_comparador
+
+#EJERCICIO 15
+#COMPARACION DE LA cad29 y cad30
+cad29="mi perro se llama machin"
+cad30="mi perro se llama machin"
+comparador=(cad29==cad30)
+print("el valor de la comparacion de ambas cadenas es: ", comparador)
+#fin_comparador
